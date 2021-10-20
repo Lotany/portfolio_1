4cc02b1dfae3d1b6a0ecf77e02d382477823b990
Tany <91387946+Lotany@users.noreply.github.com>
2021-10-20 08:56:43 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $to = "lotanable2@gmail.com";
 $subject = "Mail From website lotany portfolio";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@lotany.com" . "\r\n" .
-"CC: lotanable2@gmail.com";
+"CC: ";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
